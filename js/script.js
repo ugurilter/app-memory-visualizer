bd83fba91a0872834ac871ad449dce04500dfdbd
Ugur ILTER <ugur.ilter@airties.com>
2020-09-10 23:07:06 +0300

    initial commit of working app

---
+++ js/script.js
@@ -0,0 +1,181 @@
+
+const chart_data = {
+    idx:0,
+    labels:[],
+    rss_dataset:[],
+    peak_dataset:[],
+    data_dataset:[],
+    is_limit_on:0,
+    printIntroduction: function() {
+        console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+    }
+};
+
+const LIM_HIST_DEP = 30;
+
+g_is_limit_on_1 = 1;
+g_is_limit_on_2 = 1;
+
+files = ['air4971.local.json', 'air4971-2.local.json'];
+
+async function draw_chart(cd)
+{
+    var ctx = $(`#chart-${cd.idx}`).get(0).getContext("2d");
+
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: cd.labels,
+            datasets: [
+                {
+                    label: 'RSS',
+                    backgroundColor: 'rgb(0, 255, 0)',
+                    borderColor: 'rgb(0, 255, 0)',
+                    data: cd.rss_dataset,
+                    fill: false,
+                    pointRadius: 2,
+					pointHoverRadius: 5
+                },
+                {
+                    label: 'Peak',
+                    backgroundColor: 'rgb(255, 0, 0)',
+                    borderColor: 'rgb(255, 0, 0)',
+                    data: cd.peak_dataset,
+                    fill: false,
+                    pointRadius: 2,
+					pointHoverRadius: 5
+                },
+                {
+                    label: 'Data',
+                    backgroundColor: 'rgb(0, 0, 255)',
+                    borderColor: 'rgb(0, 0, 255)',
+                    data: cd.data_dataset,
+                    fill: false,
+                    pointRadius: 2,
+					pointHoverRadius: 5
+                }
+            ]
+        },
+
+        options: {
+            responsive: true,
+            animation: {
+                duration: 0
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'timestamp'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: ''
+                    }
+                }]
+            }
+        }
+    });
+}
+
+async function update_chart()
+{
+    await fetch_data();
+
+    for (i = 0; i < 2; i++)  {
+        $(`#chart-${i + 1}`).replaceWith(`<canvas id="chart-${i + 1}"></canvas>`);
+        $('.chartjs-size-monitor').remove();
+        draw_chart(chartdata_list[i]);
+    }
+    
+    setTimeout (update_chart, 1000);
+}
+
+$(document).ready(function() {
+    update_chart();
+
+    $('#limit-1').prop("checked", true);
+    $('#limit-2').prop("checked", true);
+
+    $('#limit-1').change(function() {
+        if(this.checked) {
+            g_is_limit_on_1 = 1;
+        } else {
+            g_is_limit_on_1 = 0;
+        }
+    });
+
+    $('#limit-2').change(function() {
+        if(this.checked) {
+            g_is_limit_on_2 = 1;
+        } else {
+            g_is_limit_on_2 = 0;
+        }   
+    });
+});
+
+function convertEpochToSpecificTimezone(epoch, offset){
+    var d = new Date(epoch);
+    var utc = d.getTime() + (d.getTimezoneOffset() * 60000);  //This converts to UTC 00:00
+    var nd = new Date(utc + (3600000*offset));
+    return nd.toLocaleString();
+}
+
+async function fetch_data() {
+    chartdata_list = []
+
+    for (i = 0; i < 2; i++) {
+        const cd = Object.create(chart_data);
+
+        const response = await fetch(files[i]);
+        const data = await response.json();
+        const start = data.length - LIM_HIST_DEP;
+
+        cd.idx = i + 1;
+
+        cd.labels = []
+        cd.rss_dataset = []
+        cd.peak_dataset = []
+        cd.data_dataset = []
+
+        j = 0;
+        data.forEach(obj => {
+            if ((i == 0 && g_is_limit_on_1) || (i == 1 && g_is_limit_on_2)) {
+                if (j++ < start) {
+                    return;
+                }
+            }
+    
+            Object.entries(obj).forEach(([key, value]) => {
+    
+                if (key === 'timestamp') {
+                    cd.labels.push(value);
+                }
+    
+                if (key === 'rss') {
+                    cd.rss_dataset.push(value);
+                } else if (key === 'peak') {
+                    cd.peak_dataset.push(value);
+                } else if (key === 'data') {
+                    cd.data_dataset.push(value);
+                }
+            });
+        });
+
+        chartdata_list.push(cd);
+    }
+
+    return 0;
+}
---
+++ memory.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+import sys
+import signal
+import time
+import json
+import telnetlib
+import requests
+from parse import parse
+
+class tn_session:
+    def __init__ (self, ip, uname, passw):
+        self.ip = ip
+        self.uname = uname
+        self.passw = passw
+
+        self.instance = telnetlib.Telnet(ip)
+        self.file = None
+        self.json_array = []
+
+
+class mem_unit:
+    def __init__(self):
+        self.peak = 0
+        self.size = 0
+        self.lck = 0
+        self.hwm = 0
+        self.rss = 0
+        self.data = 0
+        self.stk = 0
+        self.exe = 0
+        self.lib = 0
+        self.pte = 0
+        self.swap = 0
+
+    def dump(self):
+        js = '{'
+        js += '"timestamp":"{}",'.format(time.strftime('%H:%M:%S'))
+        js += '"peak":{},'.format(self.peak)
+        js += '"size":{},'.format(self.size)
+        js += '"lck":{},'.format(self.lck)
+        js += '"hwm":{},'.format(self.hwm)
+        js += '"rss":{},'.format(self.rss)
+        js += '"data":{},'.format(self.data)
+        js += '"stk":{},'.format(self.stk)
+        js += '"exe":{},'.format(self.exe)
+        js += '"lib":{},'.format(self.lib)
+        js += '"pte":{},'.format(self.pte)
+        js += '"swap":{}'.format(self.swap)
+        js += '}'
+        return json.loads(js)
+
+    def parse(self, resp):
+        for i in range(len(resp) - 1):
+            val = parse('{}:{} kB', resp[i])
+            res = int(str.strip(str(val[1])))
+
+            if 'Peak' in val[0]:
+                unit.peak = res
+            elif 'Size' in val[0]:
+                unit.size = res
+            elif 'Lck' in val[0]:
+                unit.lck = res
+            elif 'HWM' in val[0]:
+                unit.hwm = res
+            elif 'RSS' in val[0]:
+                unit.rss = res
+            elif 'Data' in val[0]:
+                unit.data = res
+            elif 'Stk' in val[0]:
+                unit.stk = res
+            elif 'Exe' in val[0]:
+                unit.exe = res
+            elif 'Lib' in val[0]:
+                unit.lib = res
+            elif 'PTE' in val[0]:
+                unit.pte = res
+            elif 'Swap' in val[0]:
+                unit.swap = res
+
+    def append_to_file(self, js, s):
+        print(json.dumps(js))
+
+        s.file = open("{}.json".format(s.ip), "w+")
+
+        s.json_array.append(json.dumps(js).replace('\'', ''))
+
+        s.file.write('[')
+        for line in s.json_array[:-1]:
+            s.file.write('{},'.format(line))
+        
+        s.file.write(s.json_array[-1])
+        s.file.write(']')
+        s.file.close()
+
+
+def telnet_login(s):
+    inst = s.instance
+    inst.read_until(b"Login: ")
+    inst.write(s.uname.encode('ascii') + b"\n")
+    inst.read_until(b"Password: ")
+    inst.write(s.passw.encode('ascii') + b"\n")
+    inst.read_until(b" > ")
+    inst.write("sh".encode('ascii') + b"\n")
+    inst.read_until(b"# ")
+    return
+
+def telnet_exec(s, cmd):
+    inst = s.instance
+    inst.write(cmd.encode('ascii') + b"\n")
+    out = inst.read_until(b"# ")
+    return out.decode('ascii')
+
+if __name__ == '__main__':
+    command = 'PID=$(pidof {}) && cat /proc/$PID/status | grep Vm'.format(sys.argv[1]);
+
+    sessions = []
+    sessions.append(tn_session('air4971.local', 'admin', 'nrhkym4938'))
+    sessions.append(tn_session('air4971-2.local', 'admin', 'hkccwd7949'))
+
+    for s in sessions:
+        telnet_login(s)
+
+    while True:
+        for s in sessions:
+            res = telnet_exec(s, command).replace('\r', '').split('\n')
+            res = res[1:-1]
+
+            unit = mem_unit()
+            unit.parse(res)
+            js = unit.dump()
+
+            unit.append_to_file(js, s)
+
+        time.sleep(1)
+
+    for s in sessions:
+        s.instance.close()
+
